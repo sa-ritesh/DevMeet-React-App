c615c11a4198192d2da4f9861265caf316f6d124
Ritesh <arora.ritesh.95@gmail.com>
2021-04-04 20:24:31 +0530

    User Created

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const User = require("../../models/User");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
 const { check, validationResult } = require("express-validator/check");
 const router = express.Router();
 
@@ -14,14 +18,48 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     console.log(req.body);
-
+    //body validation
+    const { name, password, email, avatar } = req.body;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("USERS ROUTE");
+    try {
+      let user = await User.findOne({ email });
+      //check if user already exists
+      if (user) {
+        return res.status(400).json({
+          errors: [
+            {
+              message: "User Already Exists",
+            },
+          ],
+        });
+      }
+      // for new user set gravatar
+
+      const avatar = await gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+      user = new User({
+        name,
+        email,
+        password,
+        avatar,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      return res.send("User Registered!!");
+    } catch (err) {
+      res.send("ERROR");
+    }
   },
 );
 module.exports = router;
