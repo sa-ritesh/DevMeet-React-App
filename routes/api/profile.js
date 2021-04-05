917ce5bd817276b2cbff9a7d36544aa1c36e6b6d
Ritesh <arora.ritesh.95@gmail.com>
2021-04-05 18:02:42 +0530

    github api connected

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const request = require("request");
+const config = require("config");
 const auth = require("../../middlewares/auth");
 const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
@@ -259,4 +261,48 @@ router.put(
     }
   },
 );
+
+//delete-education
+router.delete("/education/:edu_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    const removeIndex = profile.education
+      .map((item) => item.id)
+      .indexOf(req.params.edu_id);
+    profile.education.splice(removeIndex, 1);
+    await profile.save();
+    return res.status(200).json(profile);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+// add github profile
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId",
+      )}&client_secret=${config.get("githubClientSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+
+    request(options, (err, response, body) => {
+      if (err) {
+        console.log(err);
+      }
+
+      if (response.statusCode !== 200) {
+        return res.status(404).send("Not Found");
+      }
+      return res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 module.exports = router;
