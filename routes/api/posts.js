abee5900bd9c20a91d379fe66b14043b67b0a518
Ritesh <arora.ritesh.95@gmail.com>
2021-04-05 21:15:32 +0530

    Create Post added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,7 +1,47 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 const router = express.Router();
+const auth = require("../../middlewares/auth");
+const User = require("../../models/User");
+const Post = require("../../models/Post");
 
-router.get("/", (req, res) => {
-  res.send("POSTS ROUTE");
+//creating and updating a post
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      return res.status(200).json(post);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send("Internal Server Error");
+    }
+  },
+);
+
+//fetch all posts
+router.get("/all", auth, async (req, res) => {
+  try {
+    const posts = await Post.find();
+    return res.status(200).json(posts);
+  } catch (err) {
+    console.log(err);
+    return res.status().json({ message: "Internal Server Error" });
+  }
 });
+
 module.exports = router;
