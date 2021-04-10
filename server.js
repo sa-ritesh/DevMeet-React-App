ed33e30b74a51315452e3d31497d44d838ecd041
Ritesh <arora.ritesh.95@gmail.com>
2021-04-10 15:22:10 +0530

    Server Error Change

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 5000;
 const path = require("path");
 // Connecting DB
 connectDB();
+app.set("port", PORT);
 //Initialising Routes
+app.get("/", function (req, res) {
+  res.redirect("/");
+});
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/auth", require("./routes/api/auth"));
