965dd159964465b9f648c5a645cfa0965ae55db3
Ritesh <arora.ritesh.95@gmail.com>
2021-04-10 16:52:29 +0530

    deploy

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,7 @@ const User = require("../../models/User");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const config = require("Config");
+const config = require("config");
 
 const { check, validationResult } = require("express-validator/check");
 const router = express.Router();
