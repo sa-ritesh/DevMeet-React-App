d759f1605500b171cb6127c465b0027957c8e06d
Ritesh <arora.ritesh.95@gmail.com>
2021-04-10 20:17:27 +0530

    Profile action error handle

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -13,15 +13,18 @@ function ProfileTop({
     user: { name, avatar },
   },
 }) {
+  {
+    console.log(social);
+  }
   return (
-    <div class="profile-top bg-primary p-2">
-      <img class="round-img my-1" src={avatar} alt="" />
-      <h1 class="large">{name}</h1>
-      <p class="lead">
+    <div className="profile-top bg-primary p-2">
+      <img className="round-img my-1" src={avatar} alt="" />
+      <h1 className="large">{name}</h1>
+      <p className="lead">
         {status} {company && <span> at {company}</span>}
       </p>
-      <p>{location && <span> at {location}</span>}</p>
-      <div class="icons my-1">
+      <p>{location && <span>{location}</span>}</p>
+      <div className="icons my-1">
         {website && (
           <a href="/#" target="_blank" rel="noopener noreferrer">
             <i className="fas fa-globe fa-2x"></i>
@@ -29,29 +32,32 @@ function ProfileTop({
         )}
         {social && social.twitter && (
           <a href={social.twitter} target="_blank" rel="noopener noreferrer">
-            <i className="fas fa-twitter fa-2x"></i>
+            <i class="fab fa-twitter-square" style={{ fontSize: "2.2rem" }}></i>
           </a>
         )}
         {social && social.facebook && (
           <a href={social.facebook} target="_blank" rel="noopener noreferrer">
-            <i className="fas fa-facebook fa-2x"></i>
+            <i
+              class="fab fa-facebook-square"
+              style={{ fontSize: "2.2rem" }}
+            ></i>
           </a>
         )}
         {social && social.linkedin && (
           <a href={social.linkedin} target="_blank" rel="noopener noreferrer">
-            <i className="fas fa-linkedin fa-2x"></i>
+            <i class="fab fa-linkedin" style={{ fontSize: "2.2rem" }}></i>
           </a>
         )}
 
         {social && social.youtube && (
           <a href={social.youtube} target="_blank" rel="noopener noreferrer">
-            <i className="fas fa-youtube fa-2x"></i>
+            <i class="fab fa-youtube" style={{ fontSize: "2.2rem" }}></i>
           </a>
         )}
 
         {social && social.instagram && (
           <a href={social.instagram} target="_blank" rel="noopener noreferrer">
-            <i className="fas fa-instagram fa-2x"></i>
+            <i class="fab fa-instagram" style={{ fontSize: "2.2rem" }}></i>
           </a>
         )}
       </div>
