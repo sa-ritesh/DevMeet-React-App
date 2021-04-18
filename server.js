30522620ee7a21cce86890f4aeb0f6ea847c47c1
Ritesh <arora.ritesh.95@gmail.com>
2021-04-18 15:49:52 +0530

    try

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app.use(express.json({ extended: false }));
 const connectDB = require("./config/db");
 const router = require("./routes/api/users");
 const PORT = process.env.PORT || 5000;
+const cors = require("cors");
 
 const path = require("path");
 // Connecting DB
@@ -23,20 +24,11 @@ app.use(function (req, res, next) {
   );
   next();
 });
-
+app.use(cors());
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
-
-//serve-static assets in production
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.solve(__dirname, "client", "build", "index.html"));
-//   });
-// }
-
 app.listen(PORT, (err) => {
   if (err) {
     console.log("Error in Listening to Port");
