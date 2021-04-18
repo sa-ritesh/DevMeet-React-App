9415a55e912c964f44beafdcb43df340e6b9f0a7
Ritesh <arora.ritesh.95@gmail.com>
2021-04-18 06:33:43 +0530

    Last commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const path = require("path");
 // Connecting DB
 connectDB();
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE",
