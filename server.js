957029759512701ce290e5845a410c4c319d1c22
Ritesh <arora.ritesh.95@gmail.com>
2021-04-18 09:40:58 +0530

    Last 2

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 //Initialising Routes
-app.options("*", cors());
+app.options("*", function (req, res) {
+  res.sendStatus(200);
+});
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/auth", require("./routes/api/auth"));
