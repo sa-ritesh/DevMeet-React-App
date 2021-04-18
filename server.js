b23ee5928d3b803dc64b617ab8322413aaea2531
Ritesh <arora.ritesh.95@gmail.com>
2021-04-18 06:31:45 +0530

    Last commit

--- server.js
+++ server.js
@@ -5,16 +5,12 @@ app.use(express.json({ extended: false }));
 const connectDB = require("./config/db");
 const router = require("./routes/api/users");
 const PORT = process.env.PORT || 5000;
-const cors = require("cors");
-var corsOptions = {
-  origin: "*",
-  optionsSuccessStatus: 200, // For legacy browser support
-};
+
 const path = require("path");
 // Connecting DB
 connectDB();
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8888");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE",
@@ -27,7 +23,6 @@ app.use(function (req, res, next) {
   next();
 });
 //Initialising Routes
-app.use(cors(corsOptions));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/auth", require("./routes/api/auth"));
